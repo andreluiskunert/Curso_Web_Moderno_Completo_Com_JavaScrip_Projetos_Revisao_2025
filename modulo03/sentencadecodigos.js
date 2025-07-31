efd51cc0396ed210f3c683081ad298cb033154ef
Andre Luis Kunert <seuemail@example.com>
2025-07-31 20:50:05 -0300

    Estou Revisando....2

--- modulo03/sentencadecodigos.js
+++ modulo03/sentencadecodigos.js
@@ -1,3 +1,18 @@
 console.log("---Sentenças de códigos---")
 console.log("Explicar....")
+{
+    {
+        console
+            .log(
+                "Estou no meio código..."
+            )
+    }
+}
+console.log("Eu...quanto estou é acordo pra responder o celular:")
+console.log("Bom dia")
+console.log('Boa tarde')
+console.log("Boa noite")
+console.log("Não tem ninguém, aqui e escretaria eletrônica...deixe seu recardo...")
+console.log("se for comprasse estamos ocupados,agora se for pra receber....")
+console.log("assim que puderemos retoremos")
 console.log("The End.")
