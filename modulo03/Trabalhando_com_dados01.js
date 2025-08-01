19be62f170045b8eb7110cd577fa236bfd07adc9
Andre Luis Kunert <seuemail@example.com>
2025-08-01 15:58:03 -0300

    BoraContinuarArevisão

---
+++ modulo03/Trabalhando_com_dados01.js
@@ -0,0 +1,22 @@
+console.log("Trabalhando com dados #01:")
+console.log("Explica... dando exemplo:")
+console.log("O caneta:")
+console.log("R$ :", 10.0)
+console.log("R$ :", 6.4)
+console.log("R$ :", 1.5)
+console.log("R$ :", 3.5)
+console.log('--------//----------//--------')
+let nome = "caneta"
+let quantidade = 10
+let preco = 6.4
+let imposto = 1.5
+let precoFinal = preco + imposto
+
+console.log("Objeto pra venda é:", nome)
+console.log(" A quantida de caneta são :", quantidade)
+console.log("O preço é :", preco, 'R$', "pagara", imposto, "sobre o valor da caneta")
+console.log("O valor com o imposto será : ", precoFinal, "R$;")
+console.log('---------------------->')
+nome = "Caneta BIC"
+console.log(nome)
+console.log("The End")
---
+++ modulo03/desafio_sentenca_bloco.js
@@ -0,0 +1,36 @@
+/*
+
+ø Exibir  os números:
+  //  Bloco 01 - impares
+  1
+  3
+  5
+  7
+  9
+  // Bloco 02 - pares
+  2
+  4
+  6
+  8
+  10
+
+*/
+console.log('A resposta é: ')
+{
+    console.log("ø Numeros Impares: ")
+    console.log(1)
+    console.log(3)
+    console.log(5)
+    console.log(7)
+    console.log(8)
+    console.log(9)
+}
+{
+    console.log("ø Numeros Pares: ")
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+
+}
