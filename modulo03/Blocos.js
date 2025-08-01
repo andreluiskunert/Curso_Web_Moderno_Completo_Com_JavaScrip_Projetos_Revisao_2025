0fa485700cd3867c5e03f74ea364445ab34ab76b
Andre Luis Kunert <seuemail@example.com>
2025-08-01 11:05:36 -0300

    bora_Revisar...

---
+++ modulo03/Blocos.js
@@ -0,0 +1,19 @@
+console.log("Bloco de Códigos:")
+console.log("Explica sobre o assunto...")
+{
+    {
+        {
+            console.log("aqui tem um bloco dentro de outros blocos...")
+        }
+    }
+}
+console.log('Mostrará o bloco nos comentários crtl + u pra ver...')
+// {
+//     {
+//         {
+//             console.log("aqui tem um bloco dentro de outros blocos...")
+//         }
+//     }
+// }
+
+console.log('The End')
---
+++ modulo03/comentarios.js
@@ -0,0 +1,6 @@
+console.log("comentários de Códigos:")
+console.log("// -> Uma Linha..")
+//bora comentar... 
+console.log("/* várias linhas de comentários */")
+/* Estou comentando aqui..... */
+console.log('The End')
