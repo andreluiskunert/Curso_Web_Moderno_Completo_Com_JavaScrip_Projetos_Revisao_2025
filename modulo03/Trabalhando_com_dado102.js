d6edc5f1825ec8640134cd85026c78eca44fb5d1
Andre Luis Kunert <seuemail@example.com>
2025-08-01 20:46:18 -0300

    boraCodar03

---
+++ modulo03/Trabalhando_com_dado102.js
@@ -0,0 +1,22 @@
+console.log("Trabalhando com dados #02:")
+console.log("Explica... dando exemplo:")
+console.log("O notebook LenovoT440p:")
+console.log("R$ :", 10.0)
+console.log("R$ :", 6.4)
+console.log("R$ :", 1.5)
+console.log("R$ :", 3.5)
+console.log('--------//----------//--------')
+let nome = "notebook LenovoT440p: "
+let quantidade = 1
+let preco = 1799.00
+let imposto = 1.5
+let precoFinal = preco + imposto
+
+console.log("Objeto pra venda é:", nome)
+console.log(" A quantida de caneta são :", quantidade)
+console.log("O preço é :", preco, 'R$', "pagara", imposto, "sobre o valor da notebook LenovoT440p;")
+console.log("O valor com o imposto será : ", precoFinal, "R$;")
+console.log('---------------------->')
+// nome = "Caneta BIC"
+// console.log(nome)
+console.log("The End")
