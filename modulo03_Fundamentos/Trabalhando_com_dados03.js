823a1803fbbf2620d4b47b4c2cf171240a40729d
Andre Luis Kunert <seuemail@example.com>
2025-08-02 11:18:41 -0300

    boraCodar

---
+++ modulo03_Fundamentos/Trabalhando_com_dados03.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com dados #03:")
+console.log("Explica... dando exemplo:")
+// Ex.:01ª
+let idade = 43
+console.log(typeof 43)
+console.log(typeof idade)
+//Ex.:02ª
+let salario = 4578.34
+console.log(typeof salario)
+//Ex.:03ª
+
+
+console.log("The End")
---
+++ modulo03_Fundamentos/Trabalhando_com_dados04.js
@@ -0,0 +1,16 @@
+console.log("Trabalhando com dados #04:")
+console.log("Explica... dando exemplo:")
+// Ex.:01ª
+//let a = 10
+const a = 3;
+//
+//a + a = 10
+//a = 10
+//
+console.log(a)
+//Ex.:02ª
+
+//Ex.:03ª
+
+
+console.log("The End")
