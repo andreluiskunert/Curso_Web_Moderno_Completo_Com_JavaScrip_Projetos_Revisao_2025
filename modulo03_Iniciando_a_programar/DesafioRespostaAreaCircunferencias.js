d0e204e0e9ebadd0299e99e1a959968117c5e3b1
Andre Luis Kunert <seuemail@example.com>
2025-08-02 22:32:10 -0300

    BoraCodarJS

---
+++ modulo03_Iniciando_a_programar/DesafioRespostaAreaCircunferencias.js
@@ -0,0 +1,13 @@
+console.log("Deasfio Resposta da Área da Circunferencias")
+// formula  -> PI * raio * raio
+// ******** -> PI = 3.141592
+const PI = 3.141592
+const raio = 10
+let areaCirc = PI * raio * raio
+console.log("O valor da área é ", areaCirc, "m²")
+console.log("Ø Outras formas de Respostas: ")
+console.log("O valor do PI é  ", Math.PI)
+areaCirc = Math.PI * raio * raio
+console.log("O valor da área é ", areaCirc, "m²")
+
+console.log("The End")
---
+++ modulo03_Iniciando_a_programar/DesafioRespostaTrocaValores.js
@@ -0,0 +1,15 @@
+console.log("Deasfio Resposta de Troca de Valores")
+let a = 7
+let b = 5
+//  depois da troca... a = 5 e b 7 
+console.log("Valores Originais:")
+console.log("Valor de A é", a)
+console.log("Valor de B é", b)
+console.log("Valores trocados:")
+let temp = a
+a = b
+b = temp
+// [a = b ], [ b = a]
+console.log("Valor de A é ", a)
+console.log("Valor de B é", b)
+console.log("The End")
