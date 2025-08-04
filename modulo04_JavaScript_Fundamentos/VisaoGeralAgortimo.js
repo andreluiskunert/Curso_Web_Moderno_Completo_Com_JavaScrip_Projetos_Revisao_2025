2ec9f76c9d2c93e144e8957d093b02bf985e37f7
Andre Luis Kunert <seuemail@example.com>
2025-08-03 22:24:41 -0300

    Bora CodarJS02

---
+++ modulo04_JavaScript_Fundamentos/VisaoGeralAgortimo.js
@@ -0,0 +1,5 @@
+console.log("Visão Geral Algoritmos")
+console.log("Explica...o que é algoritmo? que é uma sequencia de passo...")
+console.log("A ordem dos passos são importante,todas as linguagens usam dados e informações...")
+
+console.log("The End")
