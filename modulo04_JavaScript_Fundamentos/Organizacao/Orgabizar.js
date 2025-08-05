194bd53d725118cc371663902850f10ecb672473
Andre Luis Kunert <seuemail@example.com>
2025-08-04 21:10:53 -0300

    Codando e Salvando4

---
+++ modulo04_JavaScript_Fundamentos/Organizacao/Orgabizar.js
@@ -0,0 +1,9 @@
+console.log("Sentença de código")
+{
+    {
+        console.log('Estou dentro de blocos...')
+        console.log('Estou Estudando :Curso Web Moderno Completo com JavaScript + Projetos  ')
+        console.log('Pra me aprofundar em JS pra entender NodeJS,React...')
+    }
+}
+console.log("The End;")
---
+++ modulo04_JavaScript_Fundamentos/Organizacao/executar.js
@@ -0,0 +1,3 @@
+console.log("Executando JS")
+console.log("Explica as principais ferramentais pra executar JS")
+console.log("The End;")
