a97a7c057b6f2cb966db1c36a6ffffdd6f4a7018
Andre Luis Kunert <seuemail@example.com>
2025-08-14 21:29:24 -0300

    Bora Codar JavaScript

---
+++ modulo06_JavaScript_Função/thisPodeVariar.js
@@ -0,0 +1,19 @@
+console.log('-------> This Pode Variar <--------')
+console.log('Explica o assunto...')
+/*
+function f1(){console.log(this === window)}
+undefined
+f1()
+true debugger eval code:1:23
+undefined
+Source map error: Error: URL constructor:  is not a valid URL.
+Stack in the worker:resolveSourceMapURL@resource://devtools/client/shared/source-map-loader/utils/fetchSourceMap.js:56:22
+getOriginalURLs@resource://devtools/client/shared/source-map-loader/source-map.js:73:24
+workerHandler/</<@resource://devtools/client/shared/worker-utils.js:115:52
+workerHandler/<@resource://devtools/client/shared/worker-utils.js:113:13
+
+Resource URL: null
+Source Map URL: null
+
+*/
+console.log('The End')
