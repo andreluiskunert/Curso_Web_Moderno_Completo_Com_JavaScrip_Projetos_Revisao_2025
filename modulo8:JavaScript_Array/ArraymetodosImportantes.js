7d6ef4c33b17cc632a9720dcc1855fd0f101df4e
Andre Luis Kunert <seuemail@example.com>
2025-08-21 16:09:02 -0300

    Bora Codar com JavaScript_2025

---
+++ modulo8:JavaScript_Array/ArraymetodosImportantes.js
@@ -0,0 +1,22 @@
+console.log(' Array Metodos Importantes:  ' )
+console.log('Explica... dando exemplo.:')
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() // massa quebrou o carro!
+pilotos.push('Verstappen')
+console.log(pilotos)
+pilotos.shift()// remove o primeiro!
+console.log(pilotos)
+pilotos.unshift('Hamilton')
+console.log(pilotos)
+// splice pode adicionar e remover eelmentos
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+// remover
+pilotos.splice(3, 1) // Massa quebrou :(
+console.log(pilotos)
+const algunsPilotos1 = pilotos.slice(2) // novo
+console.log(algunsPilotos1)
+const algunsPilotos2 = pilotos.slice(1 ,4 )
+console.log(algunsPilotos2)
+
+console.log('The End')
