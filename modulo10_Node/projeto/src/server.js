2a9b17b26ea2689e49f794f37af4463af277086a
Andre Luis Kunert <seuemail@example.com>
2025-08-28 21:26:14 -0300

    Bora Codar JavaScript_2025

--- modulo10_Node/projeto/src/server.js
+++ modulo10_Node/projeto/src/server.js
@@ -1,13 +1,40 @@
 const porta = 3003
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
 // 
-// app.get('/produtos', (req, res, next) =>{
-//      console.log('Middleware 1...')
-//      next() 
-// })
+app.use(bodyParser.urlencoded( {extended : true}))
+
 app.get('/produtos', (req, res, next) =>{
-    res.send({nome:'Notebook',marca: 'Lenovo',modelo:'T44p', preco: 1799.00}) // conveter para JSON
+  res.send(bancoDeDados.getProdutos())
+})
+app.get('/produtos/:id', (req, res, next) =>{
+        res.send(bancoDeDados.getProduto(req.params.id))
+
+})
+app.post('/produtos', (req, res, next) =>{
+  const produto = bancoDeDados.salvarProduto({
+    nome : req.body.name,
+    preco : req.body.preco,
+    marca : req.body.marca,
+    modelo : req.body.modelo
+  })  
+  res.send(produto) // Gerar Um JSON
+})
+app.put('/produtos/:id', (req, res, next) =>{
+  const produto = bancoDeDados.salvarProduto({
+    id: req.params.id,
+    nome : req.body.name,
+    preco : req.body.preco,
+    marca : req.body.marca,
+    modelo : req.body.modelo
+  })  
+  res.send(produto) 
+})
+app.delete('/produtos/:id', (req, res, next) =>{
+  const produto = bancoDeDados.excluirProduto(req.params.id )  
+  res.send(produto) 
 })
 app.listen(porta,()=>{
     console.log(`Servidor conectado à  porta: ${porta}`)
