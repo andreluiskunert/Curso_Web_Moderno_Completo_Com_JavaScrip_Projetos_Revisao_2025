2ab7144f82c32d7369d69b0311254b4735489e3d
Andre Luis Kunert <seuemail@example.com>
2025-09-02 21:31:35 -0300

    Codando JavaScript_2025

--- modulo11_ESNext/promise_01.js
+++ modulo11_ESNext/promise_01.js
@@ -1,11 +1,20 @@
 let a = 1
 console.log(a)
 //
-let p = new Promise(function(cumprirPromessa)  {
-    cumprirPromessa(3)
+let p = new Promise(function (cumprirPromessa) {
+    // cumprirPromessa1(3, 4) 
+      cumprirPromessa(['Ana', 'Alias', 'Luis', 'Eli'])
 })
-p.then(function(valor) {
-    cumprirPromessa(['Ana', 'Alias', 'Luis', 'Eli'])
-})
-.then(valor => console.log(valor))
+function primeiroElemento(array){
+    return array[0]
+}
+function primeiraLetra(string){
+    return string[0]
+}
+const letraMinuscula =letra => letra.toLowerCase()
+p
+ .then(primeiroElemento)
+.then(primeiraLetra)
+.then(letraMinuscula)
+.then(console.log)
 .then()
