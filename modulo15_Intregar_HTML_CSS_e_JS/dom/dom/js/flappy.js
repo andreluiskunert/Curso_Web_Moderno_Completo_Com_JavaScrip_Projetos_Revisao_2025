552e17b7b6e31c379b301f51116f2d1f823fa420
Andre Luis Kunert <seuemail@example.com>
2025-11-15 10:53:24 -0300

    bora codar com JS_2025_Revisão

--- modulo15_Intregar_HTML_CSS_e_JS/dom/dom/js/flappy.js
+++ modulo15_Intregar_HTML_CSS_e_JS/dom/dom/js/flappy.js
@@ -97,12 +97,22 @@ function Passaro(alturaJogo){
     this.setY(alturaJogo / 2)
 
 }
-// const barreiras = new Barreiras(700, 1200, 200, 400)
-// const passaro = new Passaro(700)
-// const areaDoJogo = document.querySelector('[wm-flappy]')
-// areaDoJogo.appendChild(passaro.elemento)
-// barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
-// setInterval(()=>{
-//     barreiras.animar()
-//     passaro.animar()
-//  }, 20)
+
+// Sábado 15/11/2025
+function Progresso(){
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos =>{
+        this.elemento.innerHTML = pontos
+    }
+     this.atualizarPontos(0)
+}
+const barreiras = new Barreiras(700, 1200, 200, 400)
+const passaro = new Passaro(700)
+const areaDoJogo = document.querySelector('[wm-flappy]')
+areaDoJogo.appendChild(passaro.elemento)
+areaDoJogo.appendChild(new Progresso().elemento)
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+setInterval(()=>{
+    barreiras.animar()
+    passaro.animar()
+ }, 20)
