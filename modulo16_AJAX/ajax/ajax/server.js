b172dddce1fd387cfa6b5f393e35244fc2c4c542
Andre Luis Kunert <seuemail@example.com>
2025-11-21 21:30:53 -0300

    bora la codar com JS_2025

--- modulo16_AJAX/ajax/ajax/server.js
+++ modulo16_AJAX/ajax/ajax/server.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+// mão na obra
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-app.get('/teste', (req, res) => res.send('OK'))
-app.listen(8081, () => console.log('executando.....'))
+
+app.get('/teste', (req, res) => res.send('ok'))
+app.listen(8080, ()=> console.log('Executando....'))
