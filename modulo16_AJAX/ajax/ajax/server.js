53d348eb04d2c2253ca98bda362cd9e88775ef38
Andre Luis Kunert <seuemail@example.com>
2025-11-24 22:44:17 -0300

    Bora La Codar comJS_2025

--- modulo16_AJAX/ajax/ajax/server.js
+++ modulo16_AJAX/ajax/ajax/server.js
@@ -3,6 +3,16 @@ const express = require('express')
 const app = express()
 // mão na obra
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.listen(8080, ()=> console.log('Executando....'))
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, '.')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+    //  fazer o restante amanhã de noite 
+})
+app.listen(8080, () => console.log('Executando....'))
