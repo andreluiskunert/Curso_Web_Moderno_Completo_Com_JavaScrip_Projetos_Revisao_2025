31d2f12b454cb9c5ed14debcdf71550f7c61b987
Andre Luis Kunert <seuemail@example.com>
2025-12-31 12:38:24 -0300

    bora la codar Js_2025_31/12_De meio

--- modulo17_Gulp/gulp_inicial/gulp/basico/gulpfile.js
+++ modulo17_Gulp/gulp_inicial/gulp/basico/gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require('gulp')
- const { series , parallet } = require('gulp') //pode se tipo
+const { series, parallel } = require('gulp') //pode se tipo
 // const series = gulp.series // ou assim
 
-const antes1 = cb =>{
-     console.log('Ano 2024')
+const antes1 = cb => {
+    console.log('Ano 2024')
     return cb()
 }
-const hoje = cb =>{
+const hoje = cb => {
     console.log('Ano 2025')
     return cb()
 }
@@ -15,16 +15,27 @@ const depois = cb => {
     return cb()
 }
 const BoraEstudar = cb => {
-    console.log('Entrar e sai ano...')
+     console.log('Entrar e sai ano...')
+    // quarta-feira,31/12/2025_De meio de 
+    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    // .pipe(imagePelaMetade())
+    // .pipe(imageEmPretoEBranco())  
+    // .pipe(transformacoaA())
+    // .pipe(transformacoaB())
+    // .pipe(transformacoaC())
+    // .pipe(transformacoaD())
+    .pipe(gulp.dest('pastaB'))
+    .pipe(gulp.dest('pastaC'))
+
     return cb()
 }
 
-function copiar(cb){
+function copiar(cb) {
     console.log('Tarefa de copiar....')
     return cb()
 }
 module.exports.default = series(
-   parallet(antes1,hoje,),
-     depois,
-   BoraEstudar,
+    parallel(antes1, hoje,),
+    depois,
+    BoraEstudar,
 )
