2b08da3d6ba3eb59d460cb4e0c713af873cc2855
Andre Luis Kunert <seuemail@example.com>
2026-01-02 15:52:52 -0300

    Ufa até que fim conseguir...

--- modulo17_Gulp/gulp_inicial/gulp/javascript/gulpfile.js
+++ modulo17_Gulp/gulp_inicial/gulp/javascript/gulpfile.js
@@ -1,22 +1,26 @@
 // Quinta-feira, 01/01/2026
-const { series } = require('gulp')
+const { series, parallel } = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
 
 function padrao(cb) {
-    gulp.src('src/**/*.js')
+    return gulp.src('src/**/*.js')
         .pipe(babel({
             comments: false,
-            presets: ["env."]
+            presets: ['@babel/preset-env']
         }))
         .pipe(uglify())
-        .on('error', err => console.log('Erro'))
+        .on('error', err => console.log(err))
         .pipe(concat('codigo.min.js'))
         .pipe(gulp.dest('build'))
+    cb()
+}
+function fim(cb) {
+    console.log('Por hoje é só pessoooaalll kkkk')
     return cb()
 }
-exports.default = series(padrao)
 
+exports.default = parallel(padrao, fim)
 
