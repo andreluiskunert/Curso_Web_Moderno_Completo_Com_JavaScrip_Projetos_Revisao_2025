dfb92188b2f46af25f0262476de7b0196191ea3d
Andre Luis Kunert <seuemail@example.com>
2026-01-05 21:24:01 -0300

    bora la codar com JS_2026

--- modulo17_Gulp/gulp_inicial/gulp/typeScript/gulpfile.js
+++ modulo17_Gulp/gulp_inicial/gulp/typeScript/gulpfile.js
@@ -2,4 +2,10 @@ const {series } = require('gulp')
 const gulp = require('gulp')
 const ts = require('gulp-typescript')
 const tsProject = ts.createProject('tsconfig.json')
-// amanhã de noite 
+// amanhã de noite_Segunda-feira, 05/01/2026
+function transformacaoTS(){
+    return tsProject.src()
+    .pipe(tsProject())
+    .pipe(gulp.dest('build'))
+}
+exports.default = series(transformacaoTS)
