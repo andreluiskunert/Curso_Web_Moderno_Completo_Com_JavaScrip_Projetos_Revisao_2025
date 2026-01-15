e97c767b8934d01620c0aa9e4785f3fe879915fa
Andre Luis Kunert <https://github.com/andreluiskunert/Curso_Angular_2_v19_2026>
2026-01-14 21:27:49 -0300

    Bora Lá Codar com JS_2026

--- modulo17_Gulp/gulp_inicial/gulp/spa/src/gulpTasks/deps.js
+++ modulo17_Gulp/gulp_inicial/gulp/spa/src/gulpTasks/deps.js
@@ -5,12 +5,19 @@
 // 1ª Lugar
 //  Explicar....
 const gulp = require('gulp')
+// Quarta-feira,14/01/2026_De noite
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
-function depsCSS(cb){
-    return cb()
+function depsCSS(){
+return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+        .pipe(uglifycss({ "uglyComments" : false }))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
-function depsFonts(cb){
-    return cb()
+function depsFonts(){
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+            .pipe(gulp.dest('build/assets/fonts'))
 }
 module.exports = {
     depsCSS,
