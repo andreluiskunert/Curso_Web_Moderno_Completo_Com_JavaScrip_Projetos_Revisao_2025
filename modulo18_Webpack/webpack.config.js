51511b05e5c24ca432278ede1e107c1439c270be
Andre Luis Kunert <https://github.com/andreluiskunert/Curso_Angular_2_v19_2026>
2026-01-17 23:38:40 -0300

    bora la estudar JS_2026

--- modulo18_Webpack/webpack.config.js
+++ modulo18_Webpack/webpack.config.js
@@ -1,5 +1,15 @@
 const webpack = require('webpack')
 module.exports = {
     mode:  'production',
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    // Sabado,17/01/2026_de noite
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    module:{
+        rules: [{
+            
+        }]
+    }
 }
