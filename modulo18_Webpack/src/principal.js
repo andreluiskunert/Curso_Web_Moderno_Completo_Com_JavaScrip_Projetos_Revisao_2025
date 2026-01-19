f59df2d96163731e4d8da026f59700656021eb74
Andre Luis Kunert <https://github.com/andreluiskunert/Curso_Angular_2_v19_2026>
2026-01-18 22:41:21 -0300

    bora la estudar JS_2026

--- modulo18_Webpack/src/principal.js
+++ modulo18_Webpack/src/principal.js
@@ -2,5 +2,7 @@
  import Pessoa  from "./pessoa";
  import './assets/css/estilo.css'
  import './modulos/moduloA'
+ import './estilos.css'
+console.log('Webpack funcionando 🚀')
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- modulo18_Webpack/webpack.config.js
+++ modulo18_Webpack/webpack.config.js
@@ -1,15 +1,43 @@
-const webpack = require('webpack')
+// const { use } = require('react')
+// const webpack = require('webpack')
+// module.exports = {
+//     mode:  'production',
+//     entry: './src/principal.js',
+//     // Sabado,17/01/2026_de noite
+//     output: {
+//         filename: 'principal.js',
+//         path: __dirname + '/public'
+//     },
+//     module:{
+//         rules: [{
+//             //  Domingo_18/01/2026_de noite
+//             test:/\.css$/,
+//             use: [
+//                 'style-loader', // Adicionar CSS a DOM injetando a tag <style>
+//                 'css-loader' // interpreta @import, url()...
+//                 ]
+
+//         }]
+//     }
+// }
+const path = require('path')
+
 module.exports = {
-    mode:  'production',
-    entry: './src/principal.js',
-    // Sabado,17/01/2026_de noite
-    output: {
-        filename: 'principal.js',
-        path: __dirname + '/public'
-    },
-    module:{
-        rules: [{
-            
-        }]
-    }
+  mode: 'production',
+  entry: './src/principal.js',
+  output: {
+    filename: 'principal.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
 }
