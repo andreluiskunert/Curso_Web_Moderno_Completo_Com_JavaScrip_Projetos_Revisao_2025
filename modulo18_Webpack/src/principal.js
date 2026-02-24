789a5b16fdaf55690a2d97f9f519e99bdee64218
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-23 22:11:36 -0300

    bora la codar com Web Moderno usando JS_2026

--- modulo18_Webpack/src/principal.js
+++ modulo18_Webpack/src/principal.js
@@ -1,4 +1,5 @@
 import Pessoa from "./pessoa";
 // const Pessoa = require('./pessoa') // CommonJS
+import './assets/css/estilo.css' // Importa o arquivo CSS para que o Webpack possa processá-lo e incluí-lo no bundle final. 
 const atendente = new Pessoa()
 console.log(atendente.cumprimentar())   
--- modulo18_Webpack/webpack.config.js
+++ modulo18_Webpack/webpack.config.js
@@ -3,4 +3,22 @@ const webpack = require('webpack');
 module.exports = {
     mode: 'development',
     entry: './src/principal.js',
+    // segunda-feira, 23/02/2026
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public',
+    },
+    module:{
+        rules: [
+            {
+                test: /\.css$/, // Expressão regular para identificar arquivos CSS
+                use: [
+                    'style-loader',// 'style-loader' para injetar o CSS no DOM e
+                     'css-loader'// Loaders para processar arquivos CSS
+                     ] 
+            }
+        ]
+        
+    }
+
 }
