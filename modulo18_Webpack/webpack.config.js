3deb8654891c371f5f37a4cc56e4e1bf63c56a51
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-26 22:17:21 -0300

    bora codar com JS_2026

--- modulo18_Webpack/webpack.config.js
+++ modulo18_Webpack/webpack.config.js
@@ -1,34 +1,47 @@
-const modoDev= process.env.NODE_ENV !== 'production'
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPluigin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optiomize-css-assets-webpack-plugin')
+const modoDev = process.env.NODE_ENV !== 'production'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-    mode: modoDev ? 'development' : 'production',
-    entry: './src/principal.js',
-    // segunda-feira, 23/02/2026
-    output: {
-        filename: 'principal.js',
-        path: __dirname + '/public',
-    },
-    plugins:[
-        new MiniCssExtractPlugin({
-            filename: 'estilo.css'
-        })
-    ],
-    module:{
-        rules: [
-            {
-                test: /\.css$/, // Expressão regular para identificar arquivos CSS
-                use: [
-                     MiniCssExtractPlugin.loader, // Use o loader do MiniCssExtractPlugin para extrair o CSS em um arquivo separado
-                    // 'style-loader',// 'style-loader' para injetar o CSS no DOM e
-                     'css-loader'// Loaders para processar arquivos CSS
-                     ] 
-            }
-        ]
-        
-    }
+  mode: modoDev ? 'development' : 'production',
+  entry: './src/principal.js',
 
+  output: {
+    filename: 'principal.js',
+    path: __dirname + '/public',
+  },
+
+  optimization: {
+    minimize: !modoDev,
+    minimizer: [
+      new TerserPlugin({ parallel: true }),
+      new CssMinimizerPlugin(),
+    ],
+  },
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+            ecma: 6,
+        },
+    }),
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'estilo.css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
 }
